1d3b80b35b1902385cac9b880b458b23bc12d48b
VivekRajbhar23 <vivekrajbhar006@gmail.com>
2026-01-08 14:19:30 +0530

    Complete design

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -6,14 +6,14 @@ function App() {
   const [counter, setcounter] = useState(0);
   return (
     <>
-         <div>
+         <div style={{ textAlign: "center", marginTop: "50px" }}>
               <h1>Counter App</h1>
               <h2>{counter}</h2>
 
-              <button onClick= {() => {setcounter(counter+1)}}>Click +</button>
-              <button onClick= {() => {setcounter(counter-1)}}>Click -</button>
+              <button onClick= {() => {setcounter(counter+1)}}>➕ Increment</button>
+              <button onClick= {() => {setcounter(counter-1)}} style={{ margin: "10px" }}>➖ Decrement</button>
 
-              <button onClick={() => setcounter(0)}>Reset</button>
+              <button onClick={() => setcounter(0)}>🔄 Reset</button>
          </div>
     </>
   )
